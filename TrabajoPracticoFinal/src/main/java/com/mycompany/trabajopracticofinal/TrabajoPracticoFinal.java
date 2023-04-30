91fc807eb216b78bf2bf16e421c00f8c9bdd80cf
Araho14 <raulhoracio10@hotmail.com>
2023-04-30 10:04:09 -0300

    Trabajo Integrador Final ArgProg4.0

---
+++ TrabajoPracticoFinal/src/main/java/com/mycompany/trabajopracticofinal/TrabajoPracticoFinal.java
@@ -0,0 +1,18 @@
+package com.mycompany.trabajopracticofinal;
+
+import static com.mycompany.trabajopracticofinal.Principal.principal;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Trabajo Practico Final - Argentina Programa 4.0 - Programador Java Inicial
+ * Alumno: Raul Aranda
+ */
+public class TrabajoPracticoFinal {
+
+    public static void main(String[] args) throws SQLException, IOException {
+       
+        principal();
+
+    }
+}
